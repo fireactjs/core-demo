88c8c3d77c53f6cb1a29e8cc4bf8f15c93a81a36
Chaoming Li <chaoming@users.noreply.github.com>
2022-11-01 08:52:03 +1100

    installed dependencies and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import firebaseConfig from "./firebaseConfig.json";
+import { pathnames, AppTemplate, AuthProvider, AuthRoutes, MainMenu, PublicTemplate, ResetPassword, SignIn, SignUp, UserMenu, UserProfile, UserUpdateEmail, UserUpdateName, UserUpdatePassword, UserDelete } from '@fireactjs/core';
+import { BrowserRouter, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import { CircularProgress, Box } from '@mui/material';
+import authMethods from "./authMethods.json";
+
+const Brand = "FIREACT";
+
+const Logo = ({size, color}) => {
+	const logoColor = color || 'warning';
+	return (
+		<LocalFireDepartmentIcon color={logoColor} fontSize={size} />
+	);
+}
+
+const Loader = ({size}) => {
+	let cpSize = "35px";
+	switch(size){
+		case "small":
+			cpSize = "30px";
+			break;
+		case "medium":
+			cpSize = "35px";
+			break;
+		case "large":
+			cpSize = "45px";
+			break;
+		default:
+			cpSize = "35px";
+			break;
+	}
+	return (
+		<Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center"}}>
+			<CircularProgress color="warning" size={cpSize} />
+			<div style={{position: "absolute" }}>
+				<Logo size={size} />
+			</div>
+		</Box>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	return (
+		<AuthProvider firebaseConfig={firebaseConfig} brand={Brand}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AuthRoutes signInPath="/sign-in" loader={<Loader size="large" />} />} >
+						<Route element={<AppTemplate logo={<Logo size="large" />} brand={Brand} toolBarMenu={<UserMenu pathnames={pathnames} />} drawerMenu={<MainMenu pathnames={pathnames}  />} />}>
+							<Route exact path="/" element={<></>} />
+							<Route exact path={pathnames.UserProfile} element={<UserProfile pathnames={pathnames} />} />
+							<Route exact path={pathnames.UserUpdateEmail} element={<UserUpdateEmail pathnames={pathnames} />} />
+							<Route exact path={pathnames.UserUpdateName} element={<UserUpdateName pathnames={pathnames} />} />
+							<Route exact path={pathnames.UserUpdatePassword} element={<UserUpdatePassword pathnames={pathnames} />} />
+							<Route exact path={pathnames.UserDelete} element={<UserDelete pathnames={pathnames} />} />
+						</Route>
+					</Route>
+					<Route element={<PublicTemplate />}>
+						<Route path="/sign-in" element={
+							<SignIn
+								logo={<Logo size="large" />}
+								pathnames={pathnames}
+								providers={authMethods}
+							/>
+						} />
+						<Route path="/sign-up" element={
+							<SignUp
+								logo={<Logo size="large" signUpUrl="/" />}
+								pathnames={pathnames}
+							/>
+						} />
+						<Route path="/reset-password" element={
+							<ResetPassword
+								logo={<Logo size="large" signUpUrl="/" />}
+								pathnames={pathnames}
+							/>
+						} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
+	)
 }
 
 export default App;
