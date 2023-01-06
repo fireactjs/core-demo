2c32649118917e9ace406ec07c2626b197d151cf
Chaoming Li <email@chaoming.li>
2023-01-07 10:59:39 +1100

    added instruction to MyComponent

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AuthContext } from "@fireactjs/core";
-import { Box, Paper } from "@mui/material";
+import { Box, Link, Paper } from "@mui/material";
 
 export const MyComponent = () => {
 
@@ -9,6 +9,10 @@ export const MyComponent = () => {
             {context => (
                 <Paper>
                     <Box p={2}>
+                        <p><strong>
+                            This is an example component to demostrate how to retrieve the user information.
+                            For details, see <Link href="https://fireactjs.com/docs/core-package/development/" target="_blank">the documentation</Link>.
+                        </strong></p>
                         <p>User ID: {context.authUser.user.uid}</p>
                         <p>Name: {context.authUser.user.displayName}</p>
                         <p>Email: {context.authUser.user.email}</p>
